9b9ec8d09f99e06453d111c597cdc6202b5c2db6
natashand <nataliya17061992@gmail.com>
2022-07-04 10:05:19 +0300

    Add Mutators to Good class

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
+use App\Enums\GoodCategory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Validation\Rules\Enum;
 
-class Good extends Model
-{
+class Good extends Model {
     use HasFactory, SoftDeletes;
+
+    const CATEGORY = [
+        GoodCategory::HOUSEHOLD => GoodCategory::HOUSEHOLD,
+        GoodCategory::COSMETIC => GoodCategory::COSMETIC,
+        GoodCategory::AUTO => GoodCategory::AUTO,
+        GoodCategory::FOOD => GoodCategory::FOOD,
+        GoodCategory::ELEECTRONICS => GoodCategory::ELEECTRONICS,
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +27,25 @@ class Good extends Model
     protected $fillable = [
         'name',
         'description',
-        'category',
+        'serial_numbeer'
     ];
+
+    protected $casts = [
+        'category' => GoodCategory::class
+    ];
+
+    /**
+     * Get the Category value
+     */
+    public function getCategoryAttribute() {
+        return self::CATEGORY[$this->attributes['category']];
+    }
+
+    /**
+     * Set the Category value
+     */
+    public function setCategoryAttribute($value) {
+        $this->attributes['category'] = $value;
+    }
+
 }
